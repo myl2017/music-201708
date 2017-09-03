cd51eeb7784a89e05b2f3edc7022997ffb8b3024
myl2017 <706695013@qq.com>
2017-09-03 10:36:19 +0800

    test
    test

--- script/script.js
+++ script/script.js
@@ -111,7 +111,8 @@ $('input#search').on('input',function(e){
         request.open('POST','./database.json')
         request.setRequestHeader("Content-Type", "application/json");
         request.onload = function () {
-            let data = JSON.parse(request.responseText)
+            let response = JSON.stringify(requset.responseText)
+            let data = JSON.parse(response)
             for(let i=0; i<data.length;i++){
                 if(data[i].name.includes(value)||data[i].singer.includes(value)||data[i].album.includes(value)){
                     let $li = `
