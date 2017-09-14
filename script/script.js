7dfde50a89bacb3e5a274475e88fd18d0c0b53f2
myl2017 <706695013@qq.com>
2017-09-14 09:07:49 +0800

    修改搜索
    修改搜索

--- script/script.js
+++ script/script.js
@@ -3,42 +3,22 @@
  */
 $(function () {
     $.get('./database.json').then(function (reponse) {
-        let playList = reponse
-        playList.forEach((i)=>{
+        reponse.forEach((i)=>{
             if(i.id <= 6){
-                let $li = $(`
-                    <li>
-                        <a href="./playlist.html?id=${i.id}">
-                            <div class="cover">
-                                <img src="${i.cover}" alt="推荐歌曲1"/>
-                                <span class="listener"><svg class="icon-headset"><use xlink:href="#icon-erjiicon"></use></svg>170万</span>
-                            </div>
-                            <p class="remd-summary">${i.description}</p>
-                        </a>
-                    </li>
-                `)
+                let $li = recommendTemplate(i)
                 $('.recommendList .recommendSongs').append($li)
             }else{
                 return
             }
         })
         $('.recommendList .loading').remove()
+    },function (error) {
+        alert('error !!!')
     })
     $.get('./database.json').then(function (response) {
-        let songs = response
-        songs.forEach((i)=>{
+        response.forEach((i)=>{
             if(i.id >= 7 && i.id <= 16){
-                let $li = $(`
-                    <li>
-                        <a href="./song.html?id=${i.id}">
-                            <div class="songInfo">
-                                <h3>${i.name}</h3>
-                                <div class="songIntroduce"><i class="icon-sq"> </i><span>${i.singer} - ${i.name}</span></div>
-                            </div>
-                            <div class="playButton"><span></span></div>
-                        </a>
-                    </li>
-                `)
+                let $li = newestTemplate(i)
                 $('.newestList .newestSongs').append($li)
             }
         })
@@ -47,52 +27,79 @@ $(function () {
         alert('error!!!')
     })
     $.get('./database.json').then(function (response) {
-        let hotSongs = response
         let index = 0
-        hotSongs.forEach((i)=>{
-            let $li
-            let $li2
+        response.forEach((i)=>{
+            let $li,$li2
             if(i.isHot === 1){
-                $li= $(`
-                    <li>
-                        <a href="./song.html?id=${i.id}">
-                            <div class="order song-hot">0${index += 1}</div>
-                            <div class="songInfo">
-                                <h3>${i.name}<span></span></h3>
-                                <div class="songIntroduce"><i class="icon-sq"> </i><span>${i.singer} - ${i.name}</span></div>
-                            </div>
-                            <div class="playButton"><span></span></div>
-                        </a>
-                    </li>
-                `)
+                $li= hotSongsTemplate(i,index)
                 $('.hotSongList .hotSongs').append($li)
             }else{
-                 $li2= $(`
-                    <li>
-                        <a href="./song.html?id=${i.id}">
-                            <div class="order">${i.id>=10 ? i.id + '' : '0' + i.id}</div>
-                            <div class="songInfo">
-                                <h3>${i.name}<span></span></h3>
-                                <div class="songIntroduce"><i class="icon-sq"> </i><span>${i.singer} - ${i.name}</span></div>
-                            </div>
-                            <div class="playButton"><span></span></div>
-                        </a>
-                    </li>
-                 `)
-                 $('.hotSongList .hotSongs').append($li2)
+                $li2= hotSongsTemplate2(i)
+                $('.hotSongList .hotSongs').append($li2)
             }
         })
         $('.hotSongList .loading').remove()
-        let $viewAll = `               
-                        <div class="viewAll">
-                              <a class="hotViewLink" href="#">查看完整榜单 <span class="greaterThan">&gt;</span></a>
-                        </div>
-                        `
-        $('.hotsonglist .hotSongs').next($viewAll)
+    },function (error) {
+        alert('error!!!')
+    })
+    $.get('./database.json').then(function (response) {
+        response.forEach((i)=>{
+            if(i.isHot === 2){
+                let $li = $(`<li><a class="link" href="./song.html?id=${i.id}">${i.hotSearch}</a></li>`)
+                $('.records .hotList .hotSearch').append($li)
+            }
+        })
     },function (error) {
         alert('error!!!')
     })
 })
+
+function recommendTemplate(i) {
+    return  $(`<li>
+                    <a href="./playlist.html?id=${i.id}">
+                         <div class="cover">
+                             <img src="${i.cover}" alt="推荐歌曲1"/>
+                             <span class="listener"><svg class="icon-headset"><use xlink:href="#icon-erjiicon"></use></svg>${i.listener}万</span>
+                         </div>
+                         <p class="remd-summary">${i.description}</p>
+                    </a>
+                </li>`)
+}
+function  newestTemplate(i) {
+    return $(`<li>
+                   <a href="./song.html?id=${i.id}">
+                        <div class="songInfo">
+                            <h3>${i.name}</h3>
+                            <div class="songIntroduce"><i class="icon-sq"> </i><span>${i.singer} - ${i.name}</span></div>
+                        </div>
+                        <div class="playButton"><span></span></div>
+                   </a>
+               </li>`)
+}
+function hotSongsTemplate(i,index) {
+    return $(`<li>
+                   <a href="./song.html?id=${i.id}">
+                        <div class="order song-hot">0${index += 1}</div>
+                            <div class="songInfo">
+                                <h3>${i.name}<span></span></h3>
+                                <div class="songIntroduce"><i class="icon-sq"> </i><span>${i.singer} - ${i.name}</span></div>
+                            </div>
+                        <div class="playButton"><span></span></div>
+                   </a>
+                </li>`)
+}
+function hotSongsTemplate2(i) {
+    return $(`<li>
+                   <a href="./song.html?id=${i.id}">
+                        <div class="order">${i.id>=10 ? i.id + '' : '0' + i.id}</div>
+                            <div class="songInfo">
+                                <h3>${i.name}<span></span></h3>
+                                <div class="songIntroduce"><i class="icon-sq"> </i><span>${i.singer} - ${i.name}</span></div>
+                            </div>
+                        <div class="playButton"><span></span></div>
+                    </a>
+              </li>`)
+}
 
 //tab
 $('.tabs-nav').on('click','li',function (e) {
@@ -111,60 +118,103 @@ $('input#search').on('input',function(e){
     timer = setTimeout(function () {
         timer = null
         $('.records .hotList').removeClass('active')
-        let $input = $(e.currentTarget)
-        let value = $input.val().trim()
-        if(value===''){return}
-        let request= new XMLHttpRequest()
-        request.open('GET','./database.json')
-        request.setRequestHeader("Content-Type", "application/json");
-        request.onload = function () {
-            let data = JSON.parse(request.responseText)
-            $('.searchResult').empty()
-            if(data === undefined){
-               $('.searchResult').append(`<p class="empty">没有找到结果</p>`)
-               return
-            }
-            for(let i=0; i<data.length;i++){
-                if(data[i].name.includes(value)||data[i].singer.includes(value)||data[i].album.includes(value)){
-                    let $li = `
-                    <li>
-                        <a  href="./song.html?id=${data[i].id}">
-                            <div class="songInfo">
-                                <h3 class="highlight">${data[i].name}</h3>
-                                <div class="songIntroduce"><span>${data[i].singer} - ${data[i].name}</span></div>
-                            </div>
-                            <div class="playButton"><span></span></div>
-                        </a>
-                    </li>
-                `
-                    let $records = `
-                        <li data-id="${data[i].id}">
-                            <svg class="icon-lishi"><use xlink:href="#icon-lishi"></use></svg>
-                            <div class="lishiInfo">
-                                <div class="lishiName"><a href="./song.html?id=${data[i].id}">${data[i].name}</a></div>
-                            </div>
-                            <svg class="icon-clear" onclick="clearRecords(${data[i].id})">
-                                 <use xlink:href="#icon-guanbixiao"></use>
-                             </svg>
-                        </li>
-                `
-                   $('.searchResult').append($li)
-                   $('.records .history .history-Info').addClass('active').append($records)
-                }
-            }
-             $('.searchResult').prepend(`<h3 class="matchTitle">最佳匹配</h3>`);
+        let value = $(e.currentTarget).val().trim()
+        if(value === ''){
+            return
+        }else{
+            searchPrepare(value)
+            ajax(value)
         }
-        request.send()
     },400)
 })
-
-function  clearRecords(id) {
-    let li = "li[data-id='"+ id +"']"
-    $(li).remove()
-}
 $('.clearInput').on('click',function(e){
     $('input#search').val('')
+    $('.matchList').removeClass('active')
+    $('.records').removeClass('active')
 })
 $('input#search').on('focus',function (e) {
     $('.holder').empty()
 })
+
+//search prepare
+function searchPrepare(value) {
+    $('.records').addClass('active')
+    $('.matchTitle').remove()
+    $('.searchResult').empty()
+    $('.matchList').addClass('active')
+    $('.matchList').prepend(`<h3 class="matchTitle">搜索“${value}”</h3>`);
+}
+function  ajax(value) {
+    let request = initAjax()
+    request.onload = function () {
+        let data = parse(request)
+        loadingSearch(data,value)
+    }
+    request.send()
+}
+function loadingSearch(data,value) {
+    for(let i=0; i<data.length;i++){
+        if(data[i].name.includes(value)||data[i].singer === value||data[i].album.includes(value)){
+            $('.searchResult').append(searchTemplate(data[i]))
+            $('.records .history .history-Info').addClass('active').append(recordsTemplate(data[i]))
+        }
+    }
+}
+function searchTemplate(song) {
+    return $(`<li data-id="${song.id}" onclick="chooseAjax(${song.id})">
+                <a  href="javascript:void(0);">
+                    <svg class="icon-search"><use xlink:href="#icon-btnserach"></use></svg>
+                    <div class="matchItem"><div class="matchSongName">${song.name}</div></div>
+                </a>
+             </li>`)
+}
+function chooseAjax(id) {
+    let request = initAjax()
+    request.onload = function () {
+        let data = parse(request)
+        for(let i=0; i<data.length;i++){
+            if(data[i].id === id){
+                $('.matchTitle').remove()
+                $('.searchResult').empty()
+                $('.searchResult').append(resultTemplate(data[i]))
+            }
+        }
+    }
+    request.send()
+}
+function resultTemplate(song) {
+    return  $(`<li>
+                 <a  href="./song.html?id=${song.id}">
+                     <div class="songInfo">
+                          <h3 class="highlight">${song.name}</h3>
+                              <div class="songIntroduce"><span>${song.singer} - ${song.name}</span></div>
+                     </div>
+                     <div class="playButton"><span></span></div>
+                 </a>
+              </li>`)
+}
+function recordsTemplate(song) {
+    return  $(`<li data-id="${song.id}">
+                 <svg class="icon-lishi"><use xlink:href="#icon-lishi"></use></svg>
+                 <div class="lishiInfo"><div class="lishiName"><a href="./song.html?id=${song.id}">${song.name}</a></div></div>
+                 <svg class="icon-clear" onclick="clearRecords(${song.id})"><use xlink:href="#icon-guanbixiao"></use></svg>
+             </li>`)
+}
+function  clearRecords(id) {
+    let li = "li[data-id='"+ id +"']"
+    $(li).remove()
+}
+function initAjax() {
+    let request= new XMLHttpRequest()
+    request.open('GET','./database.json')
+    request.setRequestHeader("Content-Type", "application/json");
+    return request
+}
+function parse(request) {
+    let data = JSON.parse(request.responseText)
+    if(data === undefined){
+        $('.searchResult').append(`<p class="empty">没有找到结果</p>`)
+        return
+    }
+    return data
+}
